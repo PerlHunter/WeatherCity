e874a5ea9c5730fa498e5291ce0fd3087d2e2b1d
Admin <mpas14142@gmail.com>
2022-08-02 18:37:42 +0300

    add view pager

--- app/src/main/java/com/example/weathercity/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weathercity/fragments/MainFragment.kt
@@ -2,17 +2,19 @@ package com.example.weathercity.fragments
 
 import android.Manifest
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import com.example.weathercity.MainViewModel
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import com.example.weathercity.RequestWeather
+import com.example.weathercity.adapters.VpAdapter
 import com.example.weathercity.databinding.FragmentMainBinding
 import com.example.weathercity.extends.isPermissionGranted
+import com.google.android.material.tabs.TabLayoutMediator
 
 const val API_KEY = "8c0085ce49ee4ee2b0e74114221607"
 
@@ -20,6 +22,15 @@ class MainFragment : Fragment() {
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
     private lateinit var request: RequestWeather
+
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Hours",
+        "Days"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,8 +45,17 @@ class MainFragment : Fragment() {
         checkPermission()
         request = RequestWeather(super.getContext())
         request.requestWeatherData("Lviv")
+        init()
     }
 
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        viewPager.adapter = adapter
+        TabLayoutMediator(tabLayout, viewPager){
+                tab, pos -> tab.text = tList[pos]
+        }.attach()
+
+    }
 
 
     private fun permissionListener(){
